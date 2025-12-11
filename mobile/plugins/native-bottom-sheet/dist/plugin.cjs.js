721e67249d7687e7d84e7dffb2dd0cbcc137aaf4
Oleh Halchenko <oleh.halchenko@appsdelivered.com>
2025-12-11 15:13:22 +0100

    innit

---
+++ mobile/plugins/native-bottom-sheet/dist/plugin.cjs.js
@@ -0,0 +1,8 @@
+'use strict';
+
+var core = require('@capacitor/core');
+
+const BottomSheet = core.registerPlugin('BottomSheet');
+
+exports.BottomSheet = BottomSheet;
+//# sourceMappingURL=plugin.cjs.js.map
---
+++ mobile/plugins/native-bottom-sheet/dist/plugin.js
@@ -0,0 +1,11 @@
+var capacitorBottomSheet = (function (exports, core) {
+	'use strict';
+
+	const BottomSheet = core.registerPlugin('BottomSheet');
+
+	exports.BottomSheet = BottomSheet;
+
+	return exports;
+
+})({}, capacitorExports);
+//# sourceMappingURL=plugin.js.map
